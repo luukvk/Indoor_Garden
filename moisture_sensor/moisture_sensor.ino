0cbbaa00c698297443a21e29588b51d5edd46645
luukvk <l.j.vankessel@utwente.nl>
2015-03-10 11:38:32 +0100

    Valve control and Moisture sensor

--- moisture_sensor/moisture_sensor.ino
+++ moisture_sensor/moisture_sensor.ino
@@ -1,11 +1,17 @@
+/*servo*/
+#include <Servo.h>
+Servo myservo;
+int pos_open = 0;
+int pos_closed = 179;
+
+/*moisture sensor*/
 const int moist_in = 0;
 const int div_Vin = 2;
 const int div_gnd = 3;
 
-const int MAX = 100; //the value here should be equal to the value of the sensor when in water
-const int MIN = 0; //the value here should be equal to the value of the sensor in the air
+int moisture; // the raw sensor value
+int moist; // the percentage of water in the soil
 
-int moisture;
 
 int SoilMoist(){
  int reading;
@@ -27,16 +33,37 @@ int SoilMoist(){
  return reading;
 }
 
+/*sensor values*/
+const int MAX = 100; //the value here should be equal to the value of the sensor when in water
+const int MIN = 0; //the value here should be equal to the value of the sensor in the air
+
+/*plant values*/
+const int plantmoist = ; // amount of moist needed for the plant
+const int plantlight = ; // amount of light needed for the plant
+
+
 void setup(){
+ myservo.attach(9);
  Serial.begin(9600); 
 }
 
-void loop(){
+void loop(void){
  moisture = SoilMoist();
- Serial.print("Moisture: ");
- Serial.print(moisture); //calculate the boundery values (in water/air)
- Serial.print((MAX-moisture)/(MAX-MIN)*100);//calculate the percentage of moist
+ moist = (MAX-moisture)/(MAX-MIN)*100 //calculate the percentage of moist
+ 
+ Serial.print("Moist: ");
+ //Serial.print(moisture); //raw sensor value for the MAX and MIN
+ Serial.print(moist);
  Serial.print("%");
  Serial.println();
+  
+ /*controling the valve with the servo*/
+ if(moist < plantmoist){
+   myservo.write(pos_open);
+ }
+ else{
+   myservo.write(pos_closed);
+ }
+ 
  delay(1000);
 }
